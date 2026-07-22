e9e20e89b35f6413cb91ab69b0d3ad3614c2d388
Raj Gupta <princeraj3962@gmail.com>
2026-07-22 22:05:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Student_Attendance_Management/',
   optimizeDeps: {
     exclude: ['face-api.js'],
   },
